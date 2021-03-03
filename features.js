dce47c07fcb1623f281881a4d0defc03898dae2e
Luca <32400999+luckyluke98@users.noreply.github.com>
2021-03-03 21:38:21 +0100

    Update features.js

--- features.js
+++ features.js
@@ -6,8 +6,6 @@ window.onload = function(){
   var validDomains;
   var susTlds = [".ga",".gq",".ml",".cf",".tk",".xyz",".pw",".cc",".club",".work",".top",".support",".bank",".info",".study",".click",".country",".stream",".gdn",".mom",".xin",".kim",".men",".loan",".download",".racing",".online",".center",".ren",".gb",".win",".review",".vip",".party",".tech",".science",".business"]
 
-  var validDomain = [];
-
   chrome.runtime.sendMessage({ from:"features_js", message:"get_current_tab" }, function(data){
       currentTab = data.response;
       chrome.storage.sync.get(function(data){
